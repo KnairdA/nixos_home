8b4b0320343c39c3608945dcb8bbd81d1627af1a
Adrian Kummerlaender <knairda@t-online.de>
2018-12-14 16:25:00 +0100

    Add majestix's resolution to XMonad config

--- gui/conf/xmonad.hs
+++ gui/conf/xmonad.hs
@@ -292,8 +292,10 @@ hideScreenBorder host (S.RationalRect x0 y0 w h) = S.RationalRect (x0-(bw/sw)) (
         sh = screenHeightOn host
 
 screenWidthOn  host = case host of
-  "obelix"  -> 1920
-  "asterix" -> 1366
+  "obelix"   -> 1920
+  "majestix" -> 1680
+  "asterix"  -> 1366
 screenHeightOn host = case host of
-  "obelix"  -> 1200
-  "asterix" -> 768
+  "obelix"   -> 1200
+  "majestix" -> 1050
+  "asterix"  -> 768
