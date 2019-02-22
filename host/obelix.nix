a9de76d55ac705e8a50decfba2f11e81fe81b294
Adrian Kummerlaender <adrian@kummerlaender.eu>
2019-02-22 22:31:25 +0100

    Add obelix specific task

--- host/obelix.nix
+++ host/obelix.nix
@@ -6,4 +6,13 @@
   ];
 
   custom.hidpi = false;
+
+  custom.tasks = {
+    "compustream_shell" = {
+      description = "compustream dev shell";
+      directory = "~/projects/dev/compustream";
+      terminal = true;
+      command = "nix-shell --command fish";
+    };
+  };
 }
