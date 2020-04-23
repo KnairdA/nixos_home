d38345cc8474c01dd2e2b368e7eb18eb3d635d99
Adrian Kummerlaender <adrian@kummerlaender.eu>
2020-04-23 14:35:17 +0200

    Remove gnvim
    Weird font scaling issues, crashes

--- gui/vim.nix
+++ gui/vim.nix
@@ -6,12 +6,8 @@
       neovim-qt = pkgs.neovim-qt.override {
         neovim = config.custom.pkgs.custom-neovim;
       };
-      gnvim = config.custom.nixpkgs-unstable.gnvim.override {
-        neovim = config.custom.pkgs.custom-neovim;
-      };
     in [
       neovim-qt
-      gnvim
       pkgs.xclip # required to access clipboard in nvim-gui
       pkgs.fzf   # required for all kinds of narrow-down-completion
     ];
@@ -22,19 +18,12 @@
       set guifont=Iosevka:h10
       autocmd FocusGained * :checktime
     '';
-
-    file.".config/nvim/gnvim.vim".text = ''
-      set title
-      set guifont=Iosevka:h10
-      autocmd FocusGained * :checktime
-    '';
   };
 
   programs.fish = {
     enable = true;
     shellAliases = {
       nvim-qt = "nvim-qt --no-ext-tabline ^ /dev/null > /dev/null";
-      gnvim = "bash -c \"gnvim --disable-ext-cmdline --disable-ext-tabline -- --cmd \\\"source ~/.config/nvim/gnvim.vim\\\" &> /dev/null & \"";
     };
   };
 }
