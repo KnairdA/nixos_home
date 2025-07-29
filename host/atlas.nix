7e178bebb03b5874f115b911c99e5bf7d50d1a21
Adrian Kummerlaender <adrian@kummerlaender.eu>
2025-07-29 15:36:17 +0200

    atlas: Tweak niri config, setup screen sharing, test sunshine

--- host/atlas.nix
+++ host/atlas.nix
@@ -3,6 +3,16 @@
 {
   imports = [
     ../gui/default.nix
-    ../gui/xmonad.nix
+    ../gui/niri.nix
   ];
+
+  home.packages = with pkgs; [
+    xwayland-satellite
+  ];
+
+  home.sessionVariables = {
+    QT_QPA_PLATFORM = "wayland";
+    GBM_BACKEND = "nvidia-drm";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+  };
 }
